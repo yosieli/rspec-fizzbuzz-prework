3818dfdb6dbd3a2045762cc63eb9a64213752394
feven <feventsegay6@gmail.com>
2019-04-16 16:35:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(num)
     return "Fizz"
   elsif num % 5==0
     return "Buzz"
-    elsif num % 3==0 && num % 5==0
+    elsif (num % 3==0 )&& (num % 5==0)
     return "fizzBuzz"
 end
 end
